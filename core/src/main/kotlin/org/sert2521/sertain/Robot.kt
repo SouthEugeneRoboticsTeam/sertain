70a31aa7ab4d6ecca0912295e3a7551c715918bf
Aidan Heller <asfheller@gmail.com>
2019-07-16 23:14:47 -0700

    Add comments

--- core/src/main/kotlin/org/sert2521/sertain/Robot.kt
+++ core/src/main/kotlin/org/sert2521/sertain/Robot.kt
@@ -56,7 +56,7 @@ enum class RobotMode {
 fun robot(configure: Robot.() -> Unit) {
     initializeWPILib()
 
-    // Tell the DS that the robot is ready to be enabled
+    // tell the DS that robot is ready to enable
     HAL.observeUserProgramStarting()
 
     val ds: DriverStation = DriverStation.getInstance()
@@ -68,11 +68,13 @@ fun robot(configure: Robot.() -> Unit) {
         val hasNewData = ds.waitForData(0.02)
 
         if (!ds.isDSAttached) {
+            // robot has disconnected
             robot.mode = RobotMode.DISCONNECTED
         }
 
         if (hasNewData) {
             if (robot.mode == null || robot.mode == RobotMode.DISCONNECTED) {
+                // robot has just connected to DS
                 if (robot.mode != null) robot.mode = RobotMode.DISABLED
                 fire(Connect)
             }
@@ -81,23 +83,27 @@ fun robot(configure: Robot.() -> Unit) {
 
             when {
                 ds.isDisabled && robot.mode != RobotMode.DISABLED -> {
+                    // robot has just been disabled
                     HAL.observeUserProgramDisabled()
                     robot.mode = RobotMode.DISABLED
                     fire(Disable)
                 }
                 ds.isAutonomous && robot.mode != RobotMode.AUTONOMOUS -> {
+                    // robot has just been set to autonomous
                     HAL.observeUserProgramAutonomous()
                     robot.mode = RobotMode.AUTONOMOUS
                     if (wasDisabled) fire(Enable)
                     fire(Auto)
                 }
                 ds.isOperatorControl && robot.mode != RobotMode.TELEOPERATED -> {
+                    // robot has just been set to teleop
                     HAL.observeUserProgramTeleop()
                     robot.mode = RobotMode.TELEOPERATED
                     if (wasDisabled) fire(Enable)
                     fire(Teleop)
                 }
                 ds.isTest && robot.mode != RobotMode.TEST -> {
+                    // robot has just been set to test
                     HAL.observeUserProgramTest()
                     robot.mode = RobotMode.TEST
                     if (wasDisabled) fire(Enable)
