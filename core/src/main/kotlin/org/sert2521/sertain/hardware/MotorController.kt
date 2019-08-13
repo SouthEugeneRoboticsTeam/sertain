d1a8284c18b496838b982968cc024985fa324f11
Aidan Heller <asfheller@gmail.com>
2019-08-13 14:13:37 -0700

    Clean up

--- core/src/main/kotlin/org/sert2521/sertain/hardware/MotorController.kt
+++ core/src/main/kotlin/org/sert2521/sertain/hardware/MotorController.kt
@@ -1,41 +1,22 @@
 package org.sert2521.sertain.hardware
 
 import org.sert2521.sertain.units.*
-import java.text.FieldPosition
 import com.ctre.phoenix.motorcontrol.ControlMode as CtreControlMode
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController as CtreMotorController
 
 class MotorController(val name: String, val ctreMotorController: CtreMotorController) {
     val encoderTicks = EncoderTicks(4096)
 
-
-    enum class ControlMode {
-        PERCENT_OUTPUT,
-        POSITION,
-        VELOCITY
-    }
-
-    fun set(controlMode: ControlMode, output: Double) {
-        ctreMotorController.set(
-                when (controlMode) {
-                    ControlMode.PERCENT_OUTPUT -> CtreControlMode.PercentOutput
-                    ControlMode.POSITION -> CtreControlMode.Position
-                    ControlMode.VELOCITY -> CtreControlMode.Velocity
-                },
-                output
-        )
-    }
-
-    fun setPercentOutput(output: Double) {
-        set(ControlMode.PERCENT_OUTPUT, output)
+    fun setPercent(output: Double) {
+        ctreMotorController.set(CtreControlMode.PercentOutput, output)
     }
 
     fun setPosition(output: Double) {
-        set(ControlMode.POSITION, output)
+        ctreMotorController.set(CtreControlMode.Position, output)
     }
 
     fun setVelocity(output: Double) {
-        set(ControlMode.VELOCITY, output)
+        ctreMotorController.set(CtreControlMode.Velocity, output)
     }
 
     fun <U : MetricUnit<Angular>> set(position: MetricValue<Angular, U>) {
