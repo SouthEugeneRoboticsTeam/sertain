adac654aec7d76e63201f028a87a37aa9bd2f955
Aidan Heller <asfheller@gmail.com>
2019-08-13 21:27:19 -0700

    Fix bugs

--- core/src/main/kotlin/org/sert2521/sertain/units/CompositeUnit.kt
+++ core/src/main/kotlin/org/sert2521/sertain/units/CompositeUnit.kt
@@ -13,16 +13,22 @@ class CompositeUnitType<OP : CompositionOperation, T1 : MetricUnitType, T2 : Met
         type2: T2
 ) : MetricUnitType()
 
-// Math is not well tested!
-class CompositeUnit<OP : CompositionOperation, T1 : MetricUnitType, T2 : MetricUnitType, T : CompositeUnitType<OP, T1, T2>>(
+// By operation is not well tested!
+class CompositeUnit<OP : CompositionOperation, T1 : MetricUnitType, T2 : MetricUnitType>(
         operation: OP,
         unit1: MetricUnit<T1>,
         unit2: MetricUnit<T2>
 ) : MetricUnit<CompositeUnitType<OP, T1, T2>>(
         CompositeUnitType(operation, unit1.type, unit2.type),
         when (operation) {
-            Per -> unit1.base * unit2.base
-            By -> unit1.base / unit2.base
+            Per -> unit1.base / unit2.base
+            By -> unit1.base * unit2.base
             else -> throw IllegalArgumentException("Unsupported unit operation")
         }
 )
+
+operator fun <T1 : MetricUnitType, T2 : MetricUnitType> MetricUnit<T1>.div(other: MetricUnit<T2>) =
+        CompositeUnit(Per, this, other)
+
+operator fun <T1 : MetricUnitType, T2 : MetricUnitType> MetricUnit<T1>.times(other: MetricUnit<T2>) =
+        CompositeUnit(By, this, other)
