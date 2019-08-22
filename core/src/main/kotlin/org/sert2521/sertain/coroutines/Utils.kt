9a9a2b1f56e7ed61ba8629a8cb0216bdeb50e036
Aidan Heller <asfheller@gmail.com>
2019-08-22 11:55:00 -0700

    doAll and doOne coroutine utilities

--- core/src/main/kotlin/org/sert2521/sertain/coroutines/Utils.kt
+++ core/src/main/kotlin/org/sert2521/sertain/coroutines/Utils.kt
@@ -1,11 +1,48 @@
 package org.sert2521.sertain.coroutines
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.coroutineContext
 
 suspend fun periodic(period: Long, delay: Long = 0, action: () -> Unit) {
     delay(delay)
     while (true) {
         action()
         delay(period)
+    }
+}
+
+suspend fun doAll(configure: ActionGroupConfigure.() -> Unit) {
+    with(ActionGroupConfigure().apply(configure)) {
+        val job = RobotScope.launch {
+            actions.forEach { launch(block = it) }
+        }
+        job.join()
+    }
+}
+
+suspend fun doOne(configure: ActionGroupConfigure.() -> Unit) {
+    with(ActionGroupConfigure().apply(configure)) {
+        val job = RobotScope.launch {
+            val scope = this
+            actions.forEach {
+                launch {
+                    try {
+                        it()
+                    } finally {
+                        scope.cancel()
+                    }
+                }
+            }
+        }
+        job.join()
+    }
+}
+
+class ActionGroupConfigure {
+    internal var actions = mutableListOf<suspend CoroutineScope.() -> Unit>()
+
+    fun action(action: suspend CoroutineScope.() -> Unit) {
+        actions.add(action)
     }
 }
