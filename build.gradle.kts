be8f204b0ed3982de514271d8cffe9a510cd735c
Aidan Heller <asfheller@gmail.com>
2019-08-31 19:14:09 -0700

    Fix broken build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,17 @@
-group = "org.sert2521.sertain"
-version = "1.0-SNAPSHOT"
+plugins {
+    kotlin("jvm") version "1.3.50"
+}
+
+allprojects {
+    group = "org.sert2521.sertain"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        jcenter()
+        maven("http://first.wpi.edu/FRC/roborio/maven/release")
+        maven("http://devsite.ctr-electronics.com/maven/release/")
+    }
+}
+
+
--- trajectories/build.gradle.kts
+++ trajectories/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("edu.wpi.first.hal", "hal-java", "2019.4.1")
     implementation("edu.wpi.first.ntcore", "ntcore-java", "2019.4.1")
     implementation("com.ctre.phoenix", "api-java", "5.14.1")
+    compile(project(":core"))
 }
 
 tasks.withType<KotlinCompile> {
