b69e291e9f3248aa9155aff5ebf4b28df248b01a
Aidan Heller <asfheller@gmail.com>
2019-09-09 10:45:56 -0700

    Trajectory

--- trajectories/src/main/kotlin/org/sert2521/sertain/pathfinding/Trajectory.kt
+++ trajectories/src/main/kotlin/org/sert2521/sertain/pathfinding/Trajectory.kt
@@ -1,5 +1,21 @@
 package org.sert2521.sertain.pathfinding
 
+class Trajectory(segments: List<Segment>, val inverted: Boolean = false, val factor: Double = 1.0) {
+    val segments = SegmentCollection(segments, inverted, factor)
+
+    val size get() = segments.size
+
+    fun toList() = segments.toList()
+
+    operator fun get(index: Int) = segments[index]
+
+    fun clone() = Trajectory(segments.toList(), inverted, factor)
+
+    operator fun plus(trajectory: Trajectory) =
+            Trajectory(segments.toList() + List(trajectory.size) { trajectory[it] }, inverted, factor)
+
+}
+
 data class Segment(
         val pos: Double,
         val vel: Double,
@@ -10,3 +26,15 @@ data class Segment(
         val x: Double,
         val y: Double
 )
+
+class SegmentCollection(private val segments: List<Segment>, val inverted: Boolean, val factor: Double) {
+    operator fun get(index: Int) = if (!inverted)
+        segments[index].factor(factor, factor, factor, factor, factor, factor, factor, factor)
+    else {
+        segments[index].factor(factor, factor, factor, factor, -factor, factor, factor, -factor)
+    }
+
+    val size get() = segments.size
+
+    fun toList() = segments
+}
