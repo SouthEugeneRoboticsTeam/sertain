248f21dc3771430c346ef2bb41c11f6cf1dab12c
Andrew Dassonville <dassonville.andrew@gmail.com>
2019-09-18 11:01:28 -0700

    Improve unit math (#15)
    * Improve unit math
    
    * Improve conversions
    
    * Make operators non-inline
    
    * Clean up MetricValue math
    
    * Use data classes

--- core/src/main/kotlin/org/sert2521/sertain/units/CompositeUnit.kt
+++ core/src/main/kotlin/org/sert2521/sertain/units/CompositeUnit.kt
@@ -5,22 +5,35 @@ sealed class CompositionOperation
 object Per : CompositionOperation()
 object By : CompositionOperation()
 
-class CompositeUnitType<OP : CompositionOperation, T1 : MetricUnitType, T2 : MetricUnitType>(
-    operation: OP,
-    type1: T1,
-    type2: T2
+data class CompositeUnitType<OP : CompositionOperation, T1 : MetricUnitType, T2 : MetricUnitType>(
+    val operation: OP,
+    val type1: T1,
+    val type2: T2
 ) : MetricUnitType()
 
 // By operation is not well tested!
-class CompositeUnit<OP : CompositionOperation, T1 : MetricUnitType, T2 : MetricUnitType>(
-    operation: OP,
-    unit1: MetricUnit<T1>,
-    unit2: MetricUnit<T2>
+data class CompositeUnit<OP : CompositionOperation, T1 : MetricUnitType, T2 : MetricUnitType>(
+    val operation: OP,
+    val unit1: MetricUnit<T1>,
+    val unit2: MetricUnit<T2>
 ) : MetricUnit<CompositeUnitType<OP, T1, T2>>(
         CompositeUnitType(operation, unit1.type, unit2.type),
         when (operation) {
             Per -> unit1.base / unit2.base
             By -> unit1.base * unit2.base
+            else -> throw IllegalArgumentException("Unsupported unit operation")
+        },
+        when (operation) {
+            Per -> if (unit1 != unit2) {
+                " ${unit1.symbol.trim()}/${unit2.symbol.trim()}"
+            } else {
+                ""
+            }
+            By -> if (unit1 != unit2) {
+                " ${unit1.symbol.trim()}-${unit2.symbol.trim()}"
+            } else {
+                " ${unit1.symbol.trim()}²"
+            }
             else -> throw IllegalArgumentException("Unsupported unit operation")
         }
 )
--- core/src/main/kotlin/org/sert2521/sertain/units/MetricUnit.kt
+++ core/src/main/kotlin/org/sert2521/sertain/units/MetricUnit.kt
@@ -2,33 +2,34 @@ package org.sert2521.sertain.units
 
 import kotlin.math.PI
 
-abstract class MetricUnit<T : MetricUnitType>(val type: T, val base: Double)
+abstract class MetricUnit<T : MetricUnitType>(val type: T, val base: Double, val symbol: String)
 
-abstract class ChronicUnit(seconds: Double) : MetricUnit<Chronic>(Chronic, seconds)
+abstract class ChronicUnit(seconds: Double, symbol: String) : MetricUnit<Chronic>(Chronic, seconds, symbol)
 
-object Seconds : ChronicUnit(1.0)
-object Minutes : ChronicUnit(60.0)
-object Milliseconds : ChronicUnit(0.001)
+object Seconds : ChronicUnit(1.0, " s")
+object Minutes : ChronicUnit(60.0, " min")
+object Milliseconds : ChronicUnit(0.001, " ms")
 
-abstract class LinearUnit(meters: Double) : MetricUnit<Linear>(Linear, meters)
+abstract class LinearUnit(meters: Double, symbol: String) : MetricUnit<Linear>(Linear, meters, symbol)
 
-object Meters : LinearUnit(1.0)
-object Millimeters : LinearUnit(0.001)
+object Meters : LinearUnit(1.0, " m")
+object Centimeters : LinearUnit(0.01, " cm")
+object Millimeters : LinearUnit(0.001, " mm")
 
-abstract class AngularUnit(radians: Double) : MetricUnit<Angular>(Angular, radians)
+abstract class AngularUnit(radians: Double, symbol: String) : MetricUnit<Angular>(Angular, radians, symbol)
 
-object Degrees : AngularUnit(PI / 180)
-object Radians : AngularUnit(1.0)
-object Revolutions : AngularUnit(PI * 2)
+object Degrees : AngularUnit(PI / 180, "°")
+object Radians : AngularUnit(1.0, " rad")
+object Revolutions : AngularUnit(PI * 2, " rev")
 
-abstract class LinearVelocityUnit(metersPerSecond: Double) : MetricUnit<LinearVelocity>(LinearVelocity, metersPerSecond)
+abstract class LinearVelocityUnit(metersPerSecond: Double, symbol: String) : MetricUnit<LinearVelocity>(LinearVelocity, metersPerSecond, symbol)
 
-object MetersPerSecond : LinearVelocityUnit(1.0)
-object MillimetersPerSecond : LinearVelocityUnit(0.001)
+object MetersPerSecond : LinearVelocityUnit(1.0, " m/s")
+object MillimetersPerSecond : LinearVelocityUnit(0.001, " mm/s")
 
-abstract class AngularVelocityUnit(radiansPerSecond: Double) : MetricUnit<AngularVelocity>(AngularVelocity, radiansPerSecond)
+abstract class AngularVelocityUnit(radiansPerSecond: Double, symbol: String) : MetricUnit<AngularVelocity>(AngularVelocity, radiansPerSecond, symbol)
 
-object RadiansPerSecond : AngularVelocityUnit(1.0)
-object DegreesPerSecond : AngularVelocityUnit(PI / 180)
-object RevolutionsPerSecond : AngularVelocityUnit(PI * 2)
-object RevolutionsPerMinute : AngularVelocityUnit((PI * 2) / 60)
+object RadiansPerSecond : AngularVelocityUnit(1.0, " rad/s")
+object DegreesPerSecond : AngularVelocityUnit(PI / 180, "°/s")
+object RevolutionsPerSecond : AngularVelocityUnit(PI * 2, " rev/s")
+object RevolutionsPerMinute : AngularVelocityUnit((PI * 2) / 60, " rev/min")
