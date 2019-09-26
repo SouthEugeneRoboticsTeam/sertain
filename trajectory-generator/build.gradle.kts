6410199a92770ecff11c549095149d4d2e6364cb
Aidan Heller <asfheller@gmail.com>
2019-09-26 10:50:34 -0700

    Maybe don't delete a bunch of files

---
+++ trajectory-generator/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    application
+    id("org.openjfx.javafxplugin")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect", "1.3.41"))
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.0-M2")
+    implementation("no.tornado", "tornadofx", "1.7.19")
+    compile(project(":trajectories"))
+}
+
+application {
+    mainClassName = "org.sert2521.pathmaker.AppKt"
+}
+
+javafx {
+    version = "13"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
--- trajectory-generator/src/main/kotlin/org/sert2521/pathmaker/views/MainView.kt
+++
@@ -1,9 +0,0 @@
-package org.sert2521.pathmaker.views
-
-import tornadofx.*
-
-class MainView : View() {
-    override val root = hbox {
-
-    }
-}
