aa3fe56bd571482e6b7204cb0398ad5526b3d62e
Aidan Heller <asfheller@gmail.com>
2019-12-15 15:25:26 -0800

    Add pidf

--- core/src/main/kotlin/org/sert2521/sertain/control/PidfConfig.kt
+++ core/src/main/kotlin/org/sert2521/sertain/control/PidfConfig.kt
@@ -6,3 +6,21 @@ open class PidfConfig {
     var kd = 0.0
     var kf = 0.0
 }
+
+class Pidf(config: PidfConfig, val dt: Double) {
+    private val kp = config.kp
+    private val ki = config.ki
+    private val kd = config.kd
+    private val kf = config.kf
+
+    private var integral = 0.0
+    private var lastError = 0.0
+
+    fun next(setPoint: Double, actual: Double): Double {
+        val error = setPoint - actual
+        integral += error * dt
+        val derivative = (error - lastError) / dt
+        lastError = error
+        return (kp * error) + (ki * integral) + (kd * derivative) + kf
+    }
+}
