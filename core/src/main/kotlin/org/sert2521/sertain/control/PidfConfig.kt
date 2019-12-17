13ebb98acfde9c3d6504a85e2cd643987db05145
Aidan Heller <asfheller@gmail.com>
2019-12-17 15:56:09 -0800

    Motion curves (#40)
    * Add motion curve
    
    * Add pidf
    
    * Timer
    
    * Change distance to double. It just makes sense TM
    
    * Add a new constructor that lets you just add the constraints without the config. It just makes sense TM
    
    * Rename stuff. It just makes sense TM
    
    * STUPID KTLINT!!! It just makes sense TM

--- core/src/main/kotlin/org/sert2521/sertain/control/PidfConfig.kt
+++
@@ -1,8 +0,0 @@
-package org.sert2521.sertain.control
-
-open class PidfConfig {
-    var kp = 0.0
-    var ki = 0.0
-    var kd = 0.0
-    var kf = 0.0
-}
--- core/src/main/kotlin/org/sert2521/sertain/utils/Utils.kt
+++ core/src/main/kotlin/org/sert2521/sertain/utils/Utils.kt
@@ -1,7 +1,19 @@
 package org.sert2521.sertain.utils
 
-fun Double.deadband(tolerance: Double): Double = if (this < tolerance && this > -tolerance) {
-    0.0
-} else {
-    this
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import org.sert2521.sertain.coroutines.periodic
+import kotlin.coroutines.coroutineContext
+
+suspend fun timer(period: Long, delay: Long, timeout: Long, action: (Long) -> Unit) {
+    val startTime = System.currentTimeMillis()
+    var time: Long
+    CoroutineScope(coroutineContext).launch {
+        periodic(period, delay) {
+            time = System.currentTimeMillis() - startTime
+            if (time < timeout) action(time)
+            else this@launch.cancel()
+        }
+    }.join()
 }
