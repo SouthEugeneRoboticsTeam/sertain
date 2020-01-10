33ec257b389aa56ba30f64feb024182f15b31ff7
Aidan Heller <asfheller@gmail.com>
2020-01-10 10:32:37 -0800

    Update everything

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,63 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktlint by configurations.creating
-
 plugins {
-    kotlin("jvm") version "1.3.50"
-    `maven-publish`
-}
-
-repositories {
-    maven("https://first.wpi.edu/FRC/roborio/maven/release")
+    kotlin("jvm") version "1.3.61"
+    java
+    maven
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect", "1.3.50"))
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.1")
-    implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.3.50")
-    implementation("edu.wpi.first.wpilibj", "wpilibj-java", "2019.4.1")
-    implementation("edu.wpi.first.hal", "hal-java", "2019.4.1")
-    implementation("edu.wpi.first.ntcore", "ntcore-java", "2019.4.1")
-    implementation("com.ctre.phoenix", "api-java", "5.14.1")
-    ktlint("com.pinterest:ktlint:0.34.2")
+    implementation(kotlin("stdlib-jdk8", "1.3.61"))
+    implementation(kotlin("reflect", "1.3.61"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
+    implementation("edu.wpi.first.wpilibj:wpilibj-java:2020.1.2")
+    implementation("edu.wpi.first.hal:hal-java:2020.1.2")
+    implementation("edu.wpi.first.wpiutil:wpiutil-java:2020.1.2")
+    implementation("edu.wpi.first.ntcore:ntcore-java:2020.1.2")
+    implementation("com.ctre.phoenix:api-java:5.14.1")
+    implementation("com.revrobotics.frc:SparkMax-java:1.4.1")
 }
 
-tasks {
-    val ktlint by creating(JavaExec::class) {
-        group = "verification"
-        description = "Check Kotlin code style."
-        classpath = configurations["ktlint"]
-        main = "com.pinterest.ktlint.Main"
-        args = listOf("src/**/*.kt")
-    }
-
-    "check" {
-        dependsOn(ktlint)
-    }
-
-    "publishToMavenLocal" {
-        dependsOn(jar)
-    }
-
-    create("ktlintFormat", JavaExec::class) {
-        group = "formatting"
-        description = "Fix Kotlin code style deviations."
-        classpath = configurations["ktlint"]
-        main = "com.pinterest.ktlint.Main"
-        args = listOf("-F", "src/**/*.kt")
-    }
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-    kotlinOptions.freeCompilerArgs += setOf("-Xuse-experimental=kotlin.Experimental")
+    kotlinOptions.jvmTarget = "11"
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            artifact("$buildDir/libs/${project.name}-${project.version}.jar")
-        }
-    }
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-XXLanguage:+InlineClasses", "-Xuse-experimental=kotlin.Experimental")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,24 @@
 rootProject.name = "sertain"
 
 include(":core")
+
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "1.3.61"
+    }
+    repositories {
+        mavenCentral()
+        jcenter()
+        maven("https://plugins.gradle.org/m2/")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> {
+                    println("Lol you are trash")
+                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+                }
+            }
+        }
+    }
+}
