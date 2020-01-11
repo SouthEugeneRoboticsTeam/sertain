9164fc7fe16bde6e59bf655d3b1cccf5d98b0a6d
Aidan Heller <asfheller@gmail.com>
2020-01-10 18:27:03 -0800

    nani

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,3 @@
 rootProject.name = "sertain"
 
 include(":core")
-
-pluginManagement {
-    repositories {
-        mavenCentral()
-        jcenter()
-        maven("https://plugins.gradle.org/m2/")
-    }
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "org.jetbrains.kotlin.jvm" -> {
-                    println("Lol you are trash")
-                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-                }
-            }
-        }
-    }
-}
