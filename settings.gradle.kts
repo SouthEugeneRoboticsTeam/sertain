f3916f2b17dd4eaac9725f946a8647f87442db12
Aidan Heller <asfheller@gmail.com>
2020-01-10 16:57:24 -0800

    Why gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,23 +2,20 @@ rootProject.name = "sertain"
 
 include(":core")
 
-//pluginManagement {
-//    plugins {
-//        kotlin("jvm") version "1.3.61"
-//    }
-//    repositories {
-//        mavenCentral()
-//        jcenter()
-//        maven("https://plugins.gradle.org/m2/")
-//    }
-//    resolutionStrategy {
-//        eachPlugin {
-//            when (requested.id.id) {
-//                "org.jetbrains.kotlin.jvm" -> {
-//                    println("Lol you are trash")
-//                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
-//                }
-//            }
-//        }
-//    }
-//}
+pluginManagement {
+    repositories {
+        mavenCentral()
+        jcenter()
+        maven("https://plugins.gradle.org/m2/")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> {
+                    println("Lol you are trash")
+                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+                }
+            }
+        }
+    }
+}
