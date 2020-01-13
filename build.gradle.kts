c977b27231d0d81e2743ed2f0a0eff64dea70b63
Aidan Heller <asfheller@gmail.com>
2020-01-13 15:58:59 -0800

    This???

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 
+plugins {
+    kotlin("jvm") version "1.3.61"
+    application
+    java
+    `maven-publish`
+}
+
 subprojects {
     group = "org.sert2521.sertain"
     version = "0.0.8"
-
-    repositories {
-        jcenter()
-        mavenCentral()
-        maven("https://frcmaven.wpi.edu/artifactory/release/")
-        maven("https://plugins.gradle.org/m2/")
-        maven("http://devsite.ctr-electronics.com/maven/release/")
-        maven("http://www.revrobotics.com/content/sw/max/sdk/maven/")
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "sertain"
 
 include(":core")
+
