bfbc5f363102f69c3bcbb307f4f5dabbaba88e1e
Aidan Heller <asfheller@gmail.com>
2020-01-13 17:52:34 -0800

    Add plugin management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,12 @@ rootProject.name = "sertain"
 
 include(":core")
 
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "org.jetbrains.kotlin.jvm") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
