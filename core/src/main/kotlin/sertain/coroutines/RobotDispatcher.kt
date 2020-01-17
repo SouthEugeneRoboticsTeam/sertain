fc76d6dd50350533807dc50df1d6cc7415a40177
Aidan Heller <asfheller@gmail.com>
2020-01-16 19:31:07 -0800

    Update deps

--- core/src/main/kotlin/sertain/coroutines/RobotDispatcher.kt
+++
@@ -1,7 +0,0 @@
-package org.sert2521.sertain.coroutines
-
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.asCoroutineDispatcher
-import java.util.concurrent.Executors
-
-val RobotDispatcher: CoroutineDispatcher = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
--- core/src/main/kotlin/sertain/events/Event.kt
+++
@@ -1,5 +0,0 @@
-package org.sert2521.sertain.events
-
-interface Event
-
-abstract class TargetedEvent<T>(val target: T) : Event
