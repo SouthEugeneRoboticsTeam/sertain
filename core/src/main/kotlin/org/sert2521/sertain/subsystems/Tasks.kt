59a32f24e272b99d2058eee73897aeb2494fbad7
Aidan Heller <asfheller@gmail.com>
2020-01-17 10:00:01 -0800

    Add short n' safe use

--- core/src/main/kotlin/org/sert2521/sertain/subsystems/Tasks.kt
+++ core/src/main/kotlin/org/sert2521/sertain/subsystems/Tasks.kt
@@ -30,6 +30,48 @@ suspend fun doTask(name: String = "ANONYMOUS_TASK", configure: TaskConfigure.() 
     }
 }
 
+suspend inline fun <reified S1: Subsystem> use(
+        cancelConflicts: Boolean = true,
+        name: String = "ANONYMOUS_TASK",
+        crossinline action: suspend CoroutineScope.(s1: S1) -> Unit
+) {
+    val s1 = access<S1>()
+    use(s1, cancelConflicts = cancelConflicts, name = name) { action(s1) }
+}
+
+suspend inline fun <reified S1: Subsystem, reified S2: Subsystem> use(
+        cancelConflicts: Boolean = true,
+        name: String = "ANONYMOUS_TASK",
+        crossinline action: suspend CoroutineScope.(s1: S1, s2: S2) -> Unit
+) {
+    val s1 = access<S1>()
+    val s2 = access<S2>()
+    use(s1, s2, cancelConflicts = cancelConflicts, name = name) { action(s1, s2) }
+}
+
+suspend inline fun <reified S1: Subsystem, reified S2: Subsystem, reified S3: Subsystem> use(
+        cancelConflicts: Boolean = true,
+        name: String = "ANONYMOUS_TASK",
+        crossinline action: suspend CoroutineScope.(s1: S1, s2: S2, s3: S3) -> Unit
+) {
+    val s1 = access<S1>()
+    val s2 = access<S2>()
+    val s3 = access<S3>()
+    use(s1, s2, s3, cancelConflicts = cancelConflicts, name = name) { action(s1, s2, s3) }
+}
+
+suspend inline fun <reified S1: Subsystem, reified S2: Subsystem, reified S3: Subsystem, reified S4: Subsystem> use(
+        cancelConflicts: Boolean = true,
+        name: String = "ANONYMOUS_TASK",
+        crossinline action: suspend CoroutineScope.(s1: S1, s2: S2, s3: S3, s4: S4) -> Unit
+) {
+    val s1 = access<S1>()
+    val s2 = access<S2>()
+    val s3 = access<S3>()
+    val s4 = access<S4>()
+    use(s1, s2, s3, s4, cancelConflicts = cancelConflicts, name = name) { action(s1, s2, s3, s4) }
+}
+
 suspend fun <R> use(
     vararg subsystems: Subsystem,
     cancelConflicts: Boolean = true,
