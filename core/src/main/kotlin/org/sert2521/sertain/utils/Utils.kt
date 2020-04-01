7d4851f5b8d4e21738d4291af7bd74c4448fd6d7
Aidan Heller <asfheller@gmail.com>
2020-04-01 16:44:27 -0700

    Improve timer

--- core/src/main/kotlin/org/sert2521/sertain/utils/Utils.kt
+++ core/src/main/kotlin/org/sert2521/sertain/utils/Utils.kt
@@ -6,14 +6,23 @@ import kotlinx.coroutines.launch
 import org.sert2521.sertain.coroutines.periodic
 import kotlin.coroutines.coroutineContext
 
-suspend fun timer(period: Long, delay: Long, timeout: Long, action: (Long) -> Unit) {
+suspend fun timer(period: Long = 20, delay: Long = 0, timeout: Long = 0, action: (Long) -> Unit) {
     val startTime = System.currentTimeMillis()
     var time: Long
-    CoroutineScope(coroutineContext).launch {
-        periodic(period, delay) {
-            time = System.currentTimeMillis() - startTime
-            if (time < timeout) action(time)
-            else this@launch.cancel()
+    if (timeout > 0) {
+        CoroutineScope(coroutineContext).launch {
+            periodic(period, delay) {
+                time = System.currentTimeMillis() - startTime
+                if (time < timeout) action(time)
+                else this@launch.cancel()
+            }
+        }
+    } else {
+        CoroutineScope(coroutineContext).launch {
+            periodic(period, delay) {
+                time = System.currentTimeMillis() - startTime
+                action(time)
+            }
         }
     }.join()
 }
