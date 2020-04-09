4a1097ec3065aa430a1ceafa30f31d27f07b2d82
Aidan Heller <asfheller@gmail.com>
2020-04-09 15:57:42 -0700

    Improve conversions

--- core/src/main/kotlin/org/sert2521/sertain/units/Conversions.kt
+++ core/src/main/kotlin/org/sert2521/sertain/units/Conversions.kt
@@ -1,6 +1,7 @@
 package org.sert2521.sertain.units
 
 fun <T : MetricUnitType, U1 : MetricUnit<T>, U2 : MetricUnit<T>> Double.convert(units: Pair<U1, U2>): Double {
+    if (units.first == units.second) return this
     return this * units.first.base / units.second.base
 }
 
@@ -8,6 +9,6 @@ fun <T : MetricUnitType> MetricValue<T>.convertTo(other: MetricUnit<T>): MetricV
     return MetricValue(other, value.convert(unit to other))
 }
 
-fun <T : MetricUnitType> MetricValue<T>.from(other: MetricUnit<T>): Double {
+infix fun <T : MetricUnitType> MetricValue<T>.from(other: MetricUnit<T>): Double {
     return value.convert(unit to other)
 }
