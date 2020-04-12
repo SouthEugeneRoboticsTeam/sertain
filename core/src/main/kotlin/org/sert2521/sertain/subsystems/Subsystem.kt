4e7f754c0c707c14d82febc32d2510c923ee774d
Aidan Heller <asfheller@gmail.com>
2020-04-11 20:52:44 -0700

    Start example

--- core/src/main/kotlin/org/sert2521/sertain/subsystems/Subsystem.kt
+++ core/src/main/kotlin/org/sert2521/sertain/subsystems/Subsystem.kt
@@ -2,6 +2,12 @@ package org.sert2521.sertain.subsystems
 
 import kotlinx.coroutines.CoroutineScope
 
+/**
+ * A container for code that represents an independently functioning part of the robot.
+ *
+ * @property default Command to run after any other command finishes.
+ * @property setup Code to run when subsystem is added using [add].
+ */
 abstract class Subsystem(val name: String? = null, var enabled: Boolean = true) {
     open suspend fun CoroutineScope.default() = Unit
 
---
+++ example/src/main/kotlin/org/sert2521/example/Main.kt
@@ -0,0 +1,7 @@
+package org.sert2521.example
+
+import org.sert2521.sertain.robot
+
+suspend fun main() = robot {
+    println("Hello, world!")
+}
